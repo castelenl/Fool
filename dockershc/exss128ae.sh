ad8f0b269485c247ca86646dbad7fbdfffdab865
castelenl <zhoukun52058@gmail.com>
2022-03-18 00:19:16 -0700

    Update exss128ae.sh

--- dockershc/exss128ae.sh
+++ dockershc/exss128ae.sh
@@ -6,8 +6,8 @@ if [[ "$(command -v workerone)" == "" ]]; then
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
     apk add --no-cache shadowsocks-libev >/dev/null 2>&1
     mv /usr/bin/ss-server /usr/bin/workerone
-    v2rayplugin_URL="$(wget -qO- https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest | grep -E "browser_download_url.*linux-amd64" | cut -f4 -d\")"
-    wget -O - $v2rayplugin_URL | tar -xz -C /usr/bin/ && mv /usr/bin/v2ray-plugin_linux_amd64 /usr/bin/workertwo && chmod +x /usr/bin/workertwo
+    v2rayplugin_URL="$(wget -qO- https://api.github.com/repos/teddysun/xray-plugin/releases/latest | grep -E "browser_download_url.*linux-amd64" | cut -f4 -d\")"
+    wget -O - $xray-plugin_URL | tar -xz -C /usr/bin/ && mv /usr/bin/xray-plugin_linux_amd64 /usr/bin/workertwo && chmod +x /usr/bin/workertwo
 else
     # start 
     workerone -s 0.0.0.0 -p 3000 -k k333qjv7P2vSdZnEd7dH -m aes-128-gcm --plugin /usr/bin/workertwo --plugin-opts "server;path=/W2x0a5k9t3Fk/" >/dev/null 2>&1
